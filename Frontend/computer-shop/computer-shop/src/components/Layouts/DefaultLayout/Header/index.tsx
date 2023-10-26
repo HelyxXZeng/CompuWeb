3ffd5bc09ea046387f3b998d6bf3e8e858ad7267
Bow Jaro <21520932@gm.uit.edu.vn>
2023-10-26 08:45:59 +0700

    Header

--- Frontend/computer-shop/computer-shop/src/components/Layouts/DefaultLayout/Header/index.tsx
+++ Frontend/computer-shop/computer-shop/src/components/Layouts/DefaultLayout/Header/index.tsx
@@ -1,5 +1,47 @@
+import './header.css';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+
+
 function Header() {
-    return <h2>This is header</h2>;
+
+    // return <>
+    //     <header className="wrapper">
+    //         <div className="inner">
+    //             <div className="logo">
+    //                 <img src="src/assets/logo.png"
+    //                     height={42} width={42} alt="Logo"></img>
+    //             </div>
+    //             <h2>Test</h2>
+    //         </div>
+    //     </header>
+    // </>;
+    return (
+        <>
+            <Navbar bg="light" data-bs-theme="light">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img src="src/assets/logo.png"
+                            height={42} width={42} alt="Logo"></img>
+                    </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Products</Nav.Link>
+                        <Nav.Link href="#features">Categories</Nav.Link>
+                        <Nav.Link href="#pricing">Orders</Nav.Link>
+                    </Nav>
+                    <Nav className="ms-nav">
+                        <Nav.Link href="#home">Profile</Nav.Link>
+                        <Nav.Link href="#features">Sign Out</Nav.Link>
+                        {/* <Nav.Link href="#pricing">Orders</Nav.Link> */}
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
 
 export default Header;
