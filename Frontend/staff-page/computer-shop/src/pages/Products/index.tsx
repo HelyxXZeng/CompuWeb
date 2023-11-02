47973cf134aec789f0497496fac581cb7f35a84d
Bow Jaro <21520932@gm.uit.edu.vn>
2023-11-02 10:09:55 +0700

    Delete Node Modules and Other Folders

--- Frontend/staff-page/computer-shop/src/pages/Products/index.tsx
+++
@@ -1,5 +0,0 @@
-function Product() {
-    return <h2>This is Product page</h2>;
-}
-
-export default Product;
--- Frontend/staff-page/computer-shop/src/pages/SignIn/index.tsx
+++
@@ -1,5 +0,0 @@
-function SignIn() {
-    return <h2>This is SignIn page</h2>;
-}
-
-export default SignIn;
