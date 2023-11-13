1646e5061e54aff10cd9b1e7b346db90f008e2d8
TamTLeader <21521408@gm.uit.edu.vn>
2023-11-13 20:00:43 +0700

    rework menubar(sidebar)

--- Frontend/admin-page/src/pages/footer/footer.tsx
+++ Frontend/admin-page/src/pages/footer/footer.tsx
@@ -2,7 +2,10 @@ import './footer.scss'
 
 const footer = () => {
   return (
-    <div className='footer'>footer</div>
+    <div className='footer'>
+      <span>Hitech Shop</span>
+      <span>© Hitech Shop Dev Team</span>
+    </div>
   )
 }
 
