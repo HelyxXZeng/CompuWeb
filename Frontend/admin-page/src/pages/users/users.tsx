165ffde7cf849ef04321252b6c8170df3785adb8
TamTLeader <21521408@gm.uit.edu.vn>
2023-11-15 20:12:31 +0700

    Done first Datatable for staff

--- Frontend/admin-page/src/pages/users/users.tsx
+++ Frontend/admin-page/src/pages/users/users.tsx
@@ -1,8 +1,65 @@
+import DataTable from '../../components/dataTable/DataTable'
+import { userRows } from '../../data';
 import './users.scss'
+import {GridColDef} from "@mui/x-data-grid"
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "firstName",
+    type: "string",
+    headerName: "First name",
+    width: 150,
+  },
+  {
+    field: "lastName",
+    type: "string",
+    headerName: "Last name",
+    width: 150,
+  },
+  {
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 200,
+  },
+  {
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
+  },
+];
 
 const users = () => {
   return (
-    <div className='users'>users</div>
+    <div className='users'>
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTable columns={columns} rows={userRows} slug='users'/>
+    </div>
   )
 }
 
