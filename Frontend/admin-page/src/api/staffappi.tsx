df7e070ce05a1fd64b92f2217d1fc411d4aebce8
TamTLeader <21521408@gm.uit.edu.vn>
2023-11-25 09:37:54 +0700

    first api code

---
+++ Frontend/admin-page/src/api/staffappi.tsx
@@ -0,0 +1,59 @@
+import axiosClient from "./axiosClient";
+
+export interface Staff {
+    Id: number,
+    Name: string,
+    Birthday: Date,
+    Gender: string,
+    IdCardNumber: number,
+    Address: string,
+    JoinDate: Date,
+    PhoneNumber: string,
+    Position: string,
+    Salary: number,
+    Other: string, //Status: ACTIVE, NONACTIVE, FIRED
+}
+
+const staffApi = {
+    getAll: (params: any) => {
+        const url = '/staffs/GetStaffs';
+        return axiosClient.get(url, {params});
+    },
+    get: (id: any) => {
+        const url = `staffs/GetStaffById?id=${id}`
+        return axiosClient.get(url);
+    },
+
+    add: async (staff: Staff) => {
+        try {
+            const response = await axiosClient.post('/staffs/Insert', staff);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    update: async (id: number, updatedStaff: Staff) => {
+        try {
+            const response = await axiosClient.put(`/staffs/Update/${id}`, updatedStaff);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    /* updateStatus:async (id :number, status: string) => {
+        //làm sau
+    } */
+
+    remove: async (id: number) => { //hạn chế dùng remove
+        try {
+            const response = await axiosClient.delete(`/staffs/Delete?id=/${id}`);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export default staffApi
