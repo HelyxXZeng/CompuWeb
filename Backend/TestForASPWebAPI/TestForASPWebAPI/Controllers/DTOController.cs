f4885c44969f069d0b67c4c96c6229a66e04a5c3
BaoHaiyah <113325458+BaoHaiyah@users.noreply.github.com>
2023-12-15 12:33:07 +0700

    fix some small details

--- Backend/TestForASPWebAPI/TestForASPWebAPI/Controllers/DTOController.cs
+++ Backend/TestForASPWebAPI/TestForASPWebAPI/Controllers/DTOController.cs
@@ -425,7 +425,7 @@ namespace TestForASPWebAPI.Controllers
             return Ok(productVariants);
         }
 
-        [HttpGet("Search/{keyword}/{start}+{count}")]
+        [HttpGet("Search/{keyword}/{start}-{count}")]
         public async Task<IActionResult> Search (string keyword, int start, int count)
         {
             HttpResponseMessage response = await _httpClient.GetAsync($"api/DTOController/GetLaptopProductTable");
@@ -441,7 +441,7 @@ namespace TestForASPWebAPI.Controllers
                 .Select(result => new Tuple<ProductVariantDTO, int>(result, Fuzz.PartialRatio(result.Name.ToLower(), keyword.ToLower())))
                 .Where(result => result.Item2 >= 40) // Adjust accuracy threshold here
                 .OrderByDescending(result => result.Item2)
-                .Skip(start).Take(count)
+                .Skip(start - 1).Take(count)
                 .ToList();
             
             return Ok(SearchResults);
