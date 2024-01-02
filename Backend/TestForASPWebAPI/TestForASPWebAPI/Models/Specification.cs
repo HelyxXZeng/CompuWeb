7281fa3ac0a7785b83450c2451f0e47b429040c6
BaoHaiyah <113325458+BaoHaiyah@users.noreply.github.com>
2024-01-02 09:37:33 +0700

    All funcs for Staff

--- Backend/TestForASPWebAPI/TestForASPWebAPI/Models/Specification.cs
+++ Backend/TestForASPWebAPI/TestForASPWebAPI/Models/Specification.cs
@@ -7,4 +7,11 @@
         public int SpecificationTypeId { get; set; }
         public string Value { get; set; }
     }
+    public class SpecificationDTO
+    {
+        public SpecificationDTO() { }
+        public int Id { get; set; }
+        public string SpecificationTypeName { get; set; }
+        public string Value { get; set; }
+    }
 }
